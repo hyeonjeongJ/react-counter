757de718965a0a900fa35741767c3401f497ab26
Hyeonjeong <lucy2166@naver.com>
2024-05-29 16:39:54 +0900

    컴포넌트로 학교별 Counter 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const 리턴값 = useState(0);
-  const count = 리턴값[0];
-  const setCount = 리턴값[1];
 
+function SchoolCounter({schoolName}) {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
+      <h2>{schoolName}</h2>
       <h1>{count}</h1>
       <button onClick={()=> setCount((cur_count) => cur_count + 1)}>증가</button>
       <button onClick={()=> setCount((cur_count) => cur_count - 1)}>감소</button>
       {count!=0 ? <button onClick={()=> setCount(0)}>초기화</button> : null}
-    
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <SchoolCounter schoolName="A 학교" />
+      <SchoolCounter schoolName="B 학교" />
+      <SchoolCounter schoolName="C 학교" />
     </div>
   );
 }
