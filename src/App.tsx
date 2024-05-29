9e3d4f7a1ca25b8b774eedd380c7e1f2fe4fb7c1
Hyeonjeong <lucy2166@naver.com>
2024-05-29 13:10:32 +0900

    초기화 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,13 @@ function App() {
   const count = 리턴값[0];
   const setCount = 리턴값[1];
 
-  const 리턴값_감소 = useState(0);
-  const count_dec = 리턴값_감소[0];
-  const setCount_dec = 리턴값_감소[1];
-
-
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={()=> setCount((cur_count) => cur_count + 1)}>증가</button>
-      <h1>{count_dec}</h1>
-      <button onClick={()=> setCount_dec((cur_count) => cur_count - 1)}>감소</button>     
+      <button onClick={()=> setCount((cur_count) => cur_count - 1)}>감소</button>
+      <button onClick={()=> setCount(0)}>초기화</button> 
     </div>
   );
 }
